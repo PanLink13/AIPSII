c56a78433f8952a549c36affd78c72b15c6b5756
WasabiPanda.Br <61789184+PanLink13@users.noreply.github.com>
2023-06-28 17:22:46 -0300

    Add files via upload

---
+++ Aula 3/Exercicio3/3 part 2/script.js
@@ -0,0 +1,20 @@
+// 3.6
+// let numero, quadrado, raiz
+// numero = Number(prompt("Número: "))
+// quadrado = numero**2
+// raiz = Math.sqrt(numero)
+// alert("Raíz Quadrada: " + raiz.toFixed(2) + " Quadrado: " + quadrado.toFixed(2))
+
+// 3.7
+// let numero1, numero2, media
+// numero1 = Number(prompt("Primeiro Número: "))
+// numero2 = Number(prompt("Segundo Número: "))
+// media = (numero1 + numero2) / 4
+// alert("Média: " + media.toFixed(2))
+
+// 3.8
+let corre1, corre2, corre3, corre4, corre5, custoUber, quantoGanhou
+corre1 = Number()
+custoUber = Number(prompt("Quanto custa o Uber: "))
+quantoGanhou = (corres * custoUber) * porDia
+alert("Quanto Ganhou: " + quantoGanhou.toFixed(2))
